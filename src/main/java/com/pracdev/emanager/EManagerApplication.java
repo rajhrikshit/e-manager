4326373faa73e9123363ed3e68db7c73b1dcdf46
Hrikshit Raj Chauhan <mnnit.hrikshit@gmail.com>
2020-08-05 03:02:06 +0530

    First Commit

---
+++ src/main/java/com/pracdev/emanager/EManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pracdev.emanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pracdev/emanager/EManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pracdev.emanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
